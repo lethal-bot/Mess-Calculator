1cc265df6e3084eb87185737dd867092f8ecb9fa
Aditya Keshari <aditya165.in@gmail.com>
2023-07-18 22:20:10 +0530

    refactored js

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 'use strict'
 const originalDiv = document.getElementById('row-1');
 const container = document.getElementById('main_content');
-const nameArr = ["Aniket","Sayak","Kunal","Avik","Papai","Sagar","Aminul","Tonmoy","Sumon","Debottam"];
+const nameArr = ["Aniket","Sayak","Kunal","Avik","Papai","Sagar","Aminul","Tonmoy","Sumon","Debottam","Aditya"];
 nameArr.sort();
 
-for(let i = 1; i<nameArr.length; i++){
-    const clonedDiv = originalDiv.cloneNode(true);
-    clonedDiv.id = clonedDiv.id.slice(0,clonedDiv.id.length-1);
-    clonedDiv.id += `${(i+1)}`;
-    const data = clonedDiv.querySelector('p');
-    data.textContent = nameArr[i];    
-    container.appendChild(clonedDiv);
+for(let i=0; i<nameArr.length;i++){
+    const html = `
+    <div id="row-1" class="row">
+        <p class="member_name" id="member${i+1}_Name">${nameArr[i]}</p>
+        <select name="meal options" class="meal_options" id="member${i+1}_meal_options" onchange="">
+            <option value="guest meal">guest meal</option>
+            <option value="bound meal">bound meal</option>
+        </select>
+        <input type="number" name="member_meal_count" class="member_meal_count" id="member${i+1}_meal_count" placeholder="total meals">
+        <input type="number" name="member_extra_meal" class="member_extra_meal" id="member${i+1}_extra_meal" placeholder="extra meals">
+        <div class="output_meal_cost" id="member${i+1}_meal_cost">-</div>
+    </div>
+    `
+    container.insertAdjacentHTML('beforeend',html);
 }
+
+// for(let i = 0; i<nameArr.length; i++){
+//     const clonedDiv = originalDiv.cloneNode(true);
+//     clonedDiv.id = clonedDiv.id.slice(0,clonedDiv.id.length-1);
+//     clonedDiv.id += `${(i+2)}`;
+//     const data = clonedDiv.querySelector('p');
+//     data.textContent = nameArr[i];    
+//     container.appendChild(clonedDiv);
+// }
+
+
 
 
 // querryselectors
@@ -19,64 +37,43 @@ for(let i = 1; i<nameArr.length; i++){
 const totalAmount = document.querySelector("#total_amount");
 const submit = document.querySelector(".Submit");
 const totalDays = document.querySelector("#total_days");
-const guestMealRate = document.querySelector(".guest_meal_rate");
-const boundMeal = document.querySelector(".bound_meal_count_output");
-const mealCharge = document.querySelector(".meal_charge_output");
+const guestMealRate = document.querySelector("#guest_meal_rate");
+const boundMeal = document.querySelector("#bound_meal_count_output");
+const mealCharge = document.querySelector("#meal_charge_output");
 
 
 
-
-const addNewMember = (name) =>{
-    return  {        
-        
+//submit functionality
+const process = submit.addEventListener('click',function(){
+    //guest meal rates
+    const totalAmountValue = totalAmount.value;
+    
+    //total amount spend
+    const guestMealRateValue = guestMealRate.value;
+    
+    //bound meal//
+    const totalDaysValue = totalDays.value;
+    if(totalDaysValue>31)alert("Total days cannot be more than 31");
+    else{
+        const boundMealValue = Number((totalDays.value*(4/3)).toFixed(2));
+        boundMeal.innerText = (totalDays.value*(4/3)).toFixed(2);
     }
-}
 
-const generateUniqueId = () => {
-    let unique = "";
-    for( let i = 0; i<10; i++) {
-        unique += Math.random()*10;
+    ////// array of objects ////////
+    const objArr = [];
+
+    for(let i=0;i<nameArr.length;i++){
+        const obj = {
+            memberName : document.querySelector(`#member${i+1}_Name`).textContent,
+            mealOption : document.querySelector(`#member${i+1}_meal_options`).value,
+            mealCount : document.querySelector(`#member${i+1}_meal_count`).value,
+            extraMeal : document.querySelector(`#member${i+1}_extra_meal`).value,
+            mealCost : document.querySelector(`#member${i+1}_meal_cost`)
+        };
+        objArr.push(obj);
     }
-    return unique;
-}
+    ///////////////////////////////////////////
 
-
-const process = submit.addEventListener('click',function(e){
-    console.log(totalAmount.value);
-    getAllData();
-});
-
-const getAllData = () => {
-    let id = "row-";
-    for(let i = 1; i <= nameArr.length; i++){
-        let tempid = id + i;
-        getDivElement(tempid);
-    }
-}
-
-const getDivElement = (id) => {
-    const myDiv = document.getElementById(id);
-    const clonedDiv = myDiv.cloneNode(true);
-
-    const nameElement = clonedDiv.querySelector('.member_name');
-    const name = nameElement.textContent;
     
-    const mealCountElement = clonedDiv.querySelector('.member_meal_count');
-    const member_meal_count = mealCountElement.value || 0;
     
-    const extraMealElement = clonedDiv.querySelector('.member_extra_meal');
-    const member_extra_meal = extraMealElement.value || 0;
-    
-    const mealOptionsElement = clonedDiv.querySelector('.meal_options');
-    const meal_options = mealOptionsElement.value;
-    
-    console.log(name);
-    console.log(member_meal_count);
-    console.log(extraMealElement);
-    console.log(meal_options);
-}
-
-function handleSelection(selectElement) {
-    const selectedValue = selectElement.value;
-    
-  }
+})
